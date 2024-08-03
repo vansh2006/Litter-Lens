d117392bcab052768bc1ca2f3f60b3f646dcaf58
Matias <77402029+hiatus770@users.noreply.github.com>
2024-08-03 10:37:43 -0400

    Python serial

---
+++ server/object_detection/server.py
@@ -0,0 +1,39 @@
+import serial
+import requests
+
+# Replace 'COM3' with the correct COM port for your Arduino
+ser = serial.Serial('COM4', 9600)
+
+serverIp = "http://localhost:8000"
+
+try:
+    while True:
+        if ser.in_waiting > 0:  # Check if there's data available to read
+            line = ser.readline().decode('utf-8').rstrip()  # Read a line and decode it
+            # print(line)  # Output the data to the console
+            if ("FILLED" in line):
+                print("FILLED!")
+                # SEnd post reuqest /setFull to server
+                url = serverIp + "/setFull"
+                x = requests.post(url)
+
+                if x.status_code == 200:
+                    print("Successfully notified server.")
+                else:
+                    print(f"Failed to notify server. Status code: {x.status_code}")
+
+
+            elif ("EMPTY" in line):
+                url = serverIp + "/setEmpty"
+                x = requests.post(url)
+
+                if x.status_code == 200:
+                    print("Successfully notified server.")
+                else:
+                    print(f"Failed to notify server. Status code: {x.status_code}")
+
+                print("NOT FILLED")
+except KeyboardInterrupt:
+    print("Program interrupted")
+finally:
+    ser.close()  # Ensure the serial port is closed when done
--- server/server.js
+++ server/server.js
@@ -4,9 +4,23 @@ const app = express();
 
 const port = process.env.PORT || 8000;
 
+var garbageState = "EMPTY"; 
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
   });
+
+
+app.get('/setFull', (req, res) => {
+    garbageState = "FULL";
+    res.send('Garbage is now full');
+  });
+
+app.get('/setEmpty', (req, res) => {
+    garbageState = "EMPTY";
+    res.send('Garbage is now empty');
+  }
+);
 
   app.listen(port, () => console.log('Example app is listening on port 8000.'));
 
