97cb088fcc3b07bf8ad8a6b2031138d9d7672a67
tmnin <abeerkashar@gmail.com>
2024-08-03 19:03:44 -0400

    fixed typo

--- client/app/(navbar)/about/page.tsx
+++ client/app/(navbar)/about/page.tsx
@@ -4,7 +4,7 @@ import logo from '../TrashTalkLogo.png';
 export default function About() {
   return <div className={styles.bg}>
       <img className={styles.imageBig} src={logo.src} alt="logo" />
-      <h1 className={styles.aboutTitle}>Whats TrashTalk?</h1>
+      <h1 className={styles.aboutTitle}>What's TrashTalk?</h1>
       <div className={styles.aboutDiv}>
         The goal of this project is to alert pedestrians when they are littering and they miss the garbage can!!! This is unacceptable so we must remind them by sending customized messages! Reminding them to PICK UP AFTER THEIR TRASH!!!
       </div>  
